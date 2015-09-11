ed1f63a298941764ca45e54b2ab0b7c092082a7f
Rick Snyder <rsnyder@anypresence.com>
2015-09-11 11:02:30 -0400

    Enhance logging functionality

--- src/main/java/com/anypresence/wsclient/Log.java
+++ src/main/java/com/anypresence/wsclient/Log.java
@@ -1,15 +1,29 @@
 package com.anypresence.wsclient;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Log {
 
+	private static final DateFormat FORMAT = new SimpleDateFormat("YYYY/MM/dd HH:mm:ss.SSS");
+	
 	public static boolean isDebugEnabled() {
 		return Boolean.getBoolean("DEBUG");
 	}
 	
 	public static void debug(String msg) {
 		if (isDebugEnabled()) {
-			System.out.println(msg);
+			info(msg);
 		}
+	}
+	
+	public static void info(String msg) {
+		System.out.println(format(msg));
+	}
+	
+	private static String format(String msg) {
+		return FORMAT.format(new Date()) + "    [soap] " + msg;
 	}
 	
 }
