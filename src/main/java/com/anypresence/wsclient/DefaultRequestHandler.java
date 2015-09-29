e25bff166b02f913ebe236d234cdcb696a98a6d3
Rick Snyder <rsnyder@anypresence.com>
2015-09-29 15:50:29 -0400

    Correctly derive getter names based on field name

--- src/main/java/com/anypresence/wsclient/DefaultRequestHandler.java
+++ src/main/java/com/anypresence/wsclient/DefaultRequestHandler.java
@@ -1,5 +1,9 @@
 package com.anypresence.wsclient;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -8,8 +12,12 @@ import java.lang.reflect.Parameter;
 
 import javax.jws.WebParam;
 import javax.jws.WebParam.Mode;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementRef;
 
 import com.google.gson.Gson;
+
+import com.sun.xml.bind.api.impl.NameConverter;
 
 public class DefaultRequestHandler extends RequestHandler {
 
@@ -32,17 +40,46 @@ public class DefaultRequestHandler extends RequestHandler {
 				}
 				
 				String paramName = webParam.name();
-				String methodName = "get" + Character.toUpperCase(paramName.charAt(0)) + paramName.substring(1);
-				Method getter;
-				try {
-					getter = requestInstance.getClass().getMethod(methodName);
-				} catch (NoSuchMethodException | SecurityException e) {
-					throw new SoapClientException("Unable to get getter method " + methodName + " due to " + e.getClass().getSimpleName() + ": " + e.getMessage(), e);
+				Field field = null;
+				String xmlEltName = null;
+				
+				for (Field f:  requestInstance.getClass().getDeclaredFields()) {
+					XmlElementRef xmlEltRef = f.getDeclaredAnnotation(XmlElementRef.class);
+					XmlElement xmlElt = f.getDeclaredAnnotation(XmlElement.class);
+					
+					if (xmlEltRef != null) {
+						if (xmlEltRef.name().equals(paramName)) {
+							field = f;
+							xmlEltName = xmlEltRef.name();
+						}
+					} else if (xmlElt != null) {
+						if (xmlElt.name().equals(paramName)) {
+							field = f;
+							xmlEltName = xmlElt.name();
+						}
+					}
 				}
+				
+				Method getter = null;
+				
+				try {
+					BeanInfo info = Introspector.getBeanInfo(requestInstance.getClass());
+					
+					PropertyDescriptor[] pds = info.getPropertyDescriptors();
+					for (PropertyDescriptor pd : pds) {
+						if (NameConverter.standard.toVariableName(xmlEltName).equals(field.getName())) {
+							getter = pd.getReadMethod();
+						}
+					}
+				} catch(IntrospectionException e) {
+					throw new SoapClientException("Unable to introspect on class " + requestInstance.getClass(), e);
+				}
+				
+				
 				try {
 					parameterValues[idx++] = getter.invoke(requestInstance);
 				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-					throw new SoapClientException("Unable to invoke getter method " + methodName + " due to " + e.getClass().getSimpleName() + ": " + e.getMessage(), e);
+					throw new SoapClientException("Unable to invoke getter method " + getter.getName() + " due to " + e.getClass().getSimpleName() + ": " + e.getMessage(), e);
 				}
 			}
 		}
@@ -73,5 +110,5 @@ public class DefaultRequestHandler extends RequestHandler {
 			throw new SoapClientException("Unable to find field of type " + operationMethod.getReturnType() + " on response object " + responseInstance);
 		}
 	}
-
+	
 }
