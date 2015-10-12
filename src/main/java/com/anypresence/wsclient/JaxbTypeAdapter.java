46911b28e12bb493c607d121fa2f8d1715542fb7
Rick Snyder <rsnyder@anypresence.com>
2015-10-12 10:09:01 -0400

    Add tests for Aeropost WSDL and tweak logic to handle JAXBElement parameter types

---
+++ src/main/java/com/anypresence/wsclient/JaxbTypeAdapter.java
@@ -0,0 +1,43 @@
+package com.anypresence.wsclient;
+
+import java.lang.reflect.Type;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+public class JaxbTypeAdapter implements JsonSerializer<JAXBElement<?>>, JsonDeserializer<JAXBElement<?>> {
+
+	private static final QName DEFAULT = QName.valueOf("##default");
+	
+	@Override
+	public JsonElement serialize(JAXBElement<?> src, Type typeOfSrc, JsonSerializationContext context) {
+		return context.serialize(src.getValue());
+	}
+	
+	@Override
+	public JAXBElement<?> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+		String baseTypeName = typeOfT.getTypeName().split("\\<|\\>")[1];
+		Class<?> baseType = null;
+		try {
+			baseType = Class.forName(baseTypeName);
+		} catch (ClassNotFoundException e) {
+			throw new JsonParseException("Unable to deserialize root type", e);
+		}
+		
+		Object a = context.deserialize(json, baseType);
+		
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		JAXBElement<?> elt = new JAXBElement(DEFAULT, baseType, a);
+		
+		return elt;
+	}
+
+
+} 
