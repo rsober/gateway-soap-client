fd208b6d30b971430058604e5dd67f73c51a647d
Khem Veasna <kveasna@gmail.com>
2016-10-19 13:14:53 -0400

    Add scheme to sign headers

--- src/main/java/com/anypresence/wsclient/utils/SecurityUtils.java
+++ src/main/java/com/anypresence/wsclient/utils/SecurityUtils.java
@@ -18,6 +18,58 @@ import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
 
 import com.anypresence.wsclient.Log;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import javax.xml.namespace.QName;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.ws.Dispatch;
+import javax.xml.ws.Service;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.http.HTTPBinding;
+
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Collections;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.security.auth.callback.CallbackHandler;
+
+import org.apache.wss4j.common.WSEncryptionPart;
+import org.apache.wss4j.common.bsp.BSPRule;
+import org.apache.wss4j.common.crypto.Crypto;
+import org.apache.wss4j.common.crypto.CryptoFactory;
+import org.apache.wss4j.common.crypto.CryptoType;
+import org.apache.wss4j.common.ext.WSSecurityException;
+import org.apache.wss4j.common.util.DOM2Writer;
+import org.apache.wss4j.common.util.KeyUtils;
+import org.apache.wss4j.common.util.XMLUtils;
+import org.apache.wss4j.dom.SOAPConstants;
+import org.apache.wss4j.dom.WSConstants;
+import org.apache.wss4j.dom.WSDataRef;
+import org.apache.wss4j.dom.common.CustomHandler;
+import org.apache.wss4j.dom.common.KeystoreCallbackHandler;
+import org.apache.wss4j.dom.common.SOAPUtil;
+import org.apache.wss4j.dom.common.SecretKeyCallbackHandler;
+import org.apache.wss4j.dom.common.SecurityTestUtil;
+import org.apache.wss4j.dom.engine.WSSConfig;
+import org.apache.wss4j.dom.engine.WSSecurityEngine;
+import org.apache.wss4j.dom.engine.WSSecurityEngineResult;
+import org.apache.wss4j.dom.handler.HandlerAction;
+import org.apache.wss4j.dom.handler.RequestData;
+import org.apache.wss4j.dom.handler.WSHandlerConstants;
+import org.apache.wss4j.dom.handler.WSHandlerResult;
+import org.apache.wss4j.dom.str.STRParser.REFERENCE_TYPE;
+import org.apache.wss4j.dom.util.WSSecurityUtil;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class SecurityUtils {
 
@@ -28,6 +80,28 @@ public class SecurityUtils {
 		headers.add(securityHeader);
 		return headers;
 	}
+
+	public static void getDispatch(String sourceFile, QName qNameService, QName qNamePort, String wsdl, String ns, String operationName) throws MalformedURLException {
+        URL wsdlURL = new URL(wsdl);
+        Service service = Service.create(wsdlURL, qNameService);
+        Dispatch<Source> disp = service.createDispatch(qNamePort, Source.class, Service.Mode.MESSAGE);
+        disp.getRequestContext().put(MessageContext.WSDL_OPERATION, new QName(ns, operationName));
+        return disp;
+    }
+
+    public static void signHeader(String userInfo) {
+    	WSSecEncrypt builder = new WSSecEncrypt();
+        builder.setUserInfo(userInfo);
+        builder.setKeyIdentifierType(WSConstants.BST_DIRECT_REFERENCE);
+        builder.setSymmetricEncAlgorithm(WSConstants.TRIPLE_DES);
+        Document doc = SOAPUtil.toSOAPPart(SOAPUtil.SAMPLE_SOAP_MSG);
+        WSSecHeader secHeader = new WSSecHeader(doc);
+        secHeader.insertSecurityHeader();
+        Document encryptedDoc = builder.build(doc, crypto, secHeader);
+
+        String outputString = XMLUtils.prettyDocumentToString(encryptedDoc);
+        return outputString;
+    }
 
 	private static String generateNonce()
 			throws NoSuchAlgorithmException, NoSuchProviderException, UnsupportedEncodingException {
